064498e3a494b72a36342dadad05d6af9d340e71
Laurent Callarec <l.callarec@gmail.com>
2016-09-14 22:53:07 +0200

    Use grid in prefs window

--- prefs.js
+++ prefs.js
@@ -18,46 +18,41 @@ function buildPrefsWidget() {
 
   let frame = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, border_width: 10});
 
-  let row1 = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, border_width: 10});
+  let grid = new Gtk.Grid();
 
-	let focusLabel = new Gtk.Label({label: "Focus duration (minutes)"});
+  grid.set_row_spacing(10);
+  grid.set_column_spacing(10);
+
+	frame.pack_start(grid, true, true, 0);
+
 	let focusSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
   focusSpin.value = _settings.get_int('focus-duration') / 60;
   focusSpin.connect("value-changed", Lang.bind (this, () => {
     setSetting("focus-duration", focusSpin.value)
   }));
 
-	let sep = new Gtk.Separator({orientation: Gtk.Orientation.HORIZONTAL});
+  grid.attach(new Gtk.Label({label: "Focus duration (minutes)"}), 0, 0, 1, 1)
+  grid.attach(focusSpin, 1, 0, 1, 1);
 
-  let row2 = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, border_width: 10});
+  grid.attach(new Gtk.Separator({orientation: Gtk.Orientation.HORIZONTAL}), 0, 1, 2, 1);
 
-	let shortBreakLabel = new Gtk.Label({label: "Short break duration (minutes)"});
-	let shortBreakSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
+  let shortBreakSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
   shortBreakSpin.value = _settings.get_int('short-break-duration') / 60
   shortBreakSpin.connect("value-changed", Lang.bind (this, () => {
     setSetting("short-break-duration", shortBreakSpin.value)
   }));
 
-  let row3 = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, border_width: 10});
+  grid.attach(new Gtk.Label({label: "Short break duration (minutes)"}), 0, 2, 1, 1)
+  grid.attach(shortBreakSpin, 1, 2, 1, 1);
 
-	let longBreakLabel = new Gtk.Label({label: "Long break duration (minutes)"});
-	let longBreakSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
+  let longBreakSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
   longBreakSpin.value = _settings.get_int('long-break-duration') / 60;
   longBreakSpin.connect("value-changed", Lang.bind (this, () => {
     setSetting("long-break-duration", longBreakSpin.value)
   }));
 
-	row1.pack_start(focusLabel, false, false, 5);
-	row1.pack_start(focusSpin, false, false, 5);
-	row2.pack_start(shortBreakLabel, false, false, 5);
-	row2.pack_start(shortBreakSpin, false, false, 5);
-  row3.pack_start(longBreakLabel, false, false, 5);
-	row3.pack_start(longBreakSpin, false, false, 5);
-
-	frame.pack_start(row1, true, true, 0);
-	frame.pack_start(sep, true, true, 0);
-	frame.pack_start(row2, true, true, 0);
-  frame.pack_start(row3, true, true, 0);
+  grid.attach(new Gtk.Label({label: "Long break duration (minutes)"}), 0, 3, 1, 1);
+  grid.attach(longBreakSpin, 1, 3, 1, 1);
 
   frame.show_all();
 
