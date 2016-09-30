77ad2c10a8e457990517b8d3c0d1a7e4405c4437
Laurent Callarec <l.callarec@gmail.com>
2016-09-30 20:48:23 +0200

    Remove global function from prefs.js

--- prefs.js
+++ prefs.js
@@ -1,3 +1,6 @@
+/* eslint no-unused-vars: ["error", { "vars": "local" }] */
+/* global buildPrefsWidget:true, init:true */
+
 /**
 * Prefs.js
 *
@@ -12,9 +15,20 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me             = ExtensionUtils.getCurrentExtension();
 const Convenience    = Me.imports.convenience;
 
-function buildPrefsWidget() {
+init = function() {
+  return null;
+}
 
-  let _settings = getSettings();
+const Settings = {
+  all: function() {
+    return Convenience.getSettings('org.gnome.shell.extensions.pomodoro');
+  },
+  set: function (setting, value) {
+    Settings.all().set_int(setting, value * 60);
+  }
+}
+
+buildPrefsWidget = function() {
 
   let frame = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, border_width: 10});
 
@@ -26,9 +40,9 @@ function buildPrefsWidget() {
 	frame.pack_start(grid, true, true, 0);
 
 	let focusSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
-  focusSpin.value = _settings.get_int('focus-duration') / 60;
+  focusSpin.value = Settings.all().get_int('focus-duration') / 60;
   focusSpin.connect("value-changed", Lang.bind (this, () => {
-    setSetting("focus-duration", focusSpin.value)
+    Settings.set("focus-duration", focusSpin.value)
   }));
 
   grid.attach(new Gtk.Label({label: "Focus duration (minutes)"}), 0, 0, 1, 1)
@@ -37,18 +51,18 @@ function buildPrefsWidget() {
   grid.attach(new Gtk.Separator({orientation: Gtk.Orientation.HORIZONTAL}), 0, 1, 2, 1);
 
   let shortBreakSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
-  shortBreakSpin.value = _settings.get_int('short-break-duration') / 60
+  shortBreakSpin.value = Settings.all().get_int('short-break-duration') / 60
   shortBreakSpin.connect("value-changed", Lang.bind (this, () => {
-    setSetting("short-break-duration", shortBreakSpin.value)
+    Settings.set("short-break-duration", shortBreakSpin.value)
   }));
 
   grid.attach(new Gtk.Label({label: "Short break duration (minutes)"}), 0, 2, 1, 1)
   grid.attach(shortBreakSpin, 1, 2, 1, 1);
 
   let longBreakSpin  = Gtk.SpinButton.new_with_range(0, 60, 1);
-  longBreakSpin.value = _settings.get_int('long-break-duration') / 60;
+  longBreakSpin.value = Settings.all().get_int('long-break-duration') / 60;
   longBreakSpin.connect("value-changed", Lang.bind (this, () => {
-    setSetting("long-break-duration", longBreakSpin.value)
+    Settings.set("long-break-duration", longBreakSpin.value)
   }));
 
   grid.attach(new Gtk.Label({label: "Long break duration (minutes)"}), 0, 3, 1, 1);
@@ -57,16 +71,4 @@ function buildPrefsWidget() {
   frame.show_all();
 
   return frame;
-}
-
-function setSetting(setting, value) {
-  getSettings().set_int(setting, value * 60);
-}
-
-function init() {
-  return null;
-}
-
-function getSettings() {
-  return Convenience.getSettings('org.gnome.shell.extensions.pomodoro');
 }
