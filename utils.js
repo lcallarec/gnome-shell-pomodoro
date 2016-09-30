d9a7597807d1de6ec6996f5c0eec64a4072cba68
Laurent Callarec <l.callarec@gmail.com>
2016-09-30 20:52:09 +0200

    Fix linter errors

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+/* exported Guid */
 /**
 * utils.js
 *
@@ -12,8 +13,8 @@ const Guid = {
 
         return Math.floor((1 + Math.random()) * 0x10000)
           .toString(16)
-          .substring(1);
-        };
+          .substring(1)
+        }
 
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
   }
